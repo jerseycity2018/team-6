3bb1645b837edfa6746b9aa9c32cf8928d80ab72
Carlos Saravia <crsaravia00@gmail.com>
2018-10-27 09:38:42 -0400

    Announcement Component

--- src/homeComponent/announcements.js
+++ src/homeComponent/announcements.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './announcements.css';
+import {Container, Row, Col} from 'reactstrap';
 
 class Announcements extends React.Component {
 	constructor() {
@@ -20,26 +21,31 @@ class Announcements extends React.Component {
 		return (
 			<div className = 'announcements'>
 				<header> 
-					<div className = 'wrapper'>
-						<h1 className="announcement"> Announcements </h1> 
-					</div>
+						<div className = 'wrapper'>
+							<h1 className="announcement"> Announcements </h1> 
+						</div>
 				</header>
-				<body>
-				<div class="row">
-					<div class="col-md-6 offset-md-3">
-						<div class = 'green-box'>
-							<h3>Updates about today's evening yoga class at 6:30 pm</h3>
-							<p>Class is cancelled</p>
-						</div>
-						<div class = 'green-box'>
-							<h3>Exciting news about opening another farm location in West 
-								8th Street, Brooklyn area</h3>
+				<Container>
+					<Row>
+						<Col sm="12" md={{ size: 6, offset: 4 }}>
+							<div class = 'green-box'>
+								<h3>Updates about today's evening yoga class at 6:30 pm</h3>
+								<p>Class is cancelled</p>
+							</div>
+						</Col>
+					</Row>
+
+					<Row>
+						<Col sm="12" md={{ size: 6, offset: 4 }}>
+							<div class = 'green-box'>
+								<h3>Exciting news about opening another farm location in West 
+									8th Street, Brooklyn area</h3>
 								<p>Please, come, check it out, and volunteer at our recently
-									opened farm in Brooklyn!</p>
-						</div>
-					</div>
-				</div>
-				</body>
+								opened farm in Brooklyn!</p>
+							</div>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		);
   }  
