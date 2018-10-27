c0d161cd8b20bdeb91063c90548f54a8fecc90e2
Carlos Saravia <crsaravia00@gmail.com>
2018-10-27 11:15:02 -0400

    finally

--- src/landingPage/firebase.js
+++ src/landingPage/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 const config = {
-    apiKey: "AIzaSyCpuseGAVsXfj3SGP74Gor03EPAuW63V0g",
-    authDomain: "produce-8ee92.firebaseapp.com",
-    databaseURL: "https://produce-8ee92.firebaseio.com",
-    projectId: "produce-8ee92",
-    storageBucket: "produce-8ee92.appspot.com",
-    messagingSenderId: "1059262553452"
+    apiKey: "env",
+    authDomain: "env",
+    databaseURL: "env",
+    projectId: "env",
+    storageBucket: "env",
+    messagingSenderId: "env"
     };
     firebase.initializeApp(config);
 
