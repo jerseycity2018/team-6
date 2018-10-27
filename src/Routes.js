5bb3b0f25418c7b09f35d809012b0dba12913a32
panncherry <maung.eainmatmay@gmail.com>
2018-10-26 22:26:30 -0400

    updated Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
-
+import home from './homeComponent/home';
 class Routes extends Component {
 	render() {
 		return (
 			<main>
 				<Switch>
-					
+					<Route exact path='/' component={home}/>
 				</Switch>
 			</main>
 		);
