020d43500aba44c63e8d86740d6e7103d2390b23
Carlos Saravia <crsaravia00@gmail.com>
2018-10-27 06:31:22 -0400

    Added firebase connection to frontend

--- src/landingPage/foodItem.js
+++ src/landingPage/foodItem.js
@@ -1,43 +1,74 @@
 import React, {Component} from 'react';
 import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
  CardSubtitle, CardBody } from 'reactstrap';
+ import firebase from './firebase';
+
+
+const cards = (foodName, foodImage) => {
+	return (
+		<div>
+			<Card>
+	        	<CardImg top width="100%"
+	        		style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+	        		src={foodImage} alt="Card image cap" />
+	      		<CardTitle
+	      			style={{padding: '10px',fontFamily: 'Noto Serif'}}
+		      		>{foodName}</CardTitle>
+	      	</Card>
+		</div>
+	)
+}
 
 class FoodItem extends Component {
 	constructor() {
 		super();
 		this.state = {
+			foodImage: "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180",
+			foodName:"",
 			items: []
 		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e) {
+  		this.setState({
+    		[e.target.name]: e.target.value
+  		});
+	}
+	componentDidMount() {
+		// const itemsRef1 = firebase.database().ref('items');
+		//   const item = {
+		//     foodImage: "https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/images/carrots.jpg?itok=Hgv8pUt7",
+		// 	foodName:"Carrot"
+		//   }
+
+		//   itemsRef1.push(item);
+		//   // this.setState({
+		//   //   currentItem: '',
+		  //   username: ''
+		  // });
+  		const itemsRef = firebase.database().ref('items');
+  		itemsRef.on('value', (snapshot) => {
+    	let items = snapshot.val();
+    	let newState = [];
+    	for (let item in items) {
+      		newState.push(
+      			cards(items[item].foodName,items[item].foodImage));
+
+    
+    	}
+    	this.setState({
+      		items: newState
+    	});
+  	});
 	}
 
 	render(){
+		let items = [];
+
 		return (
 			<div className = 'FoodItem'>
 				<CardColumns>
-					<Card>
-			        	<CardImg top width="100%"
-			        		style={{ width: '100%', height: '300px' }}
-			        		src="https://www.potato.ie/media/1047/potato11.jpg?anchor=center&mode=crop&width=650&height=345&rnd=130880311760000000&Format=Jpeg" alt="Card image cap" />
-			      		<CardTitle
-			      			style={{padding: '10px',fontFamily: 'Noto Serif'}}
-			      		>Potatos</CardTitle>
-			      	</Card>
-			      	<Card>
-			        	<CardImg top width="100%" 
-			        	style={{ width: '100%', height: '300px' }}
-			        	src="https://www.healthline.com/hlcmsresource/images/AN_images/AN313-Tomatoes-732x549-Thumb.jpg" alt="Card image cap" />
-			      		<CardTitle
-			      		style={{padding: '10px',fontFamily: 'Noto Serif'}}
-			      		>Tomatoes</CardTitle>
-			      	</Card>
-			      	<Card>
-			        	<CardImg top width="100%" 
-			        		style={{ width: '100%', height: '300px' }}
-			        		src="https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/images/carrots.jpg?itok=Hgv8pUt7" alt="Card image cap" />
-			      		<CardTitle
-			      			style={{padding: '10px',fontFamily: 'Noto Serif'}}
-			      		>Carrot</CardTitle>
-			      	</Card>
+					{this.state.items}
 				</CardColumns>
 			</div>
 		);
