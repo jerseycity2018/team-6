0d2d78d5a7499451514f9f3dcafb0ec8b5a4bc56
abigail <abanting00@gmail.com>
2018-10-26 21:40:19 -0400

    Setup Routes and Dependencies

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+class Routes extends Component {
+	render() {
+		return (
+			<main>
+				<Switch>
+					
+				</Switch>
+			</main>
+		);
+	}
+};
+
+export default Routes;
