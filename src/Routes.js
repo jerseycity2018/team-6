b1e5aa27bb595c34e7609f8d03b3b0ed812411f4
LENOVO-PC\diana_000 <mariaj4867@gmail.com>
2018-10-27 08:12:12 -0400

    removed excessive code

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import home from './homeComponent/home';
 import LandingPage from './landingPage/landingPage';
-import event from './landingPage/event';
 import UserContributions from './landingPage/usercontributions';
 
 class Routes extends Component {
@@ -13,7 +12,6 @@ class Routes extends Component {
 					<Route exact path='/home' component={home}/>
 					<Route exact path='/landingpage' component={LandingPage}/>
 					<Route exact path='/usercontributions' component={UserContributions}/>
-					<Route exact path='/event' component={event}/>
 				</Switch>
 			</main>
 		);
