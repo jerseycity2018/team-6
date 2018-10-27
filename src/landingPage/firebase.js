c62ecb00daf6aca78ca3d5ce60467930f379f6e2
panncherry <maung.eainmatmay@gmail.com>
2018-10-27 05:53:20 -0400

    Fix merged conflict

--- src/landingPage/firebase.js
+++ src/landingPage/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
- Initialize Firebase
-   const config = {
-       apiKey: "AIzaSyCpuseGAVsXfj3SGP74Gor03EPAuW63V0g",
-           authDomain: "produce-8ee92.firebaseapp.com",
-               databaseURL: "https://produce-8ee92.firebaseio.com",
-                   projectId: "produce-8ee92",
-                       storageBucket: "produce-8ee92.appspot.com",
-                           messagingSenderId: "1059262553452"
-                             };
-                               firebase.initializeApp(config);
+const config = {
+    apiKey: "AIzaSyCpuseGAVsXfj3SGP74Gor03EPAuW63V0g",
+    authDomain: "produce-8ee92.firebaseapp.com",
+    databaseURL: "https://produce-8ee92.firebaseio.com",
+    projectId: "produce-8ee92",
+    storageBucket: "produce-8ee92.appspot.com",
+    messagingSenderId: "1059262553452"
+    };
+    firebase.initializeApp(config);
 
 export default firebase;
 
--- src/landingPage/usercontributions.js
+++ src/landingPage/usercontributions.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import {Doughnut} from 'react-chartjs-2';
 
-var DonutChart = require("react-chartjs").Doughnut;
+var DonutChart = require('react-chartjs').Doughnut;
 var data = [
     {
         value: 300,
