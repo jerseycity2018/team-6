0ca07bf873d22eab80d15302f3487a5f5c36cec9
panncherry <maung.eainmatmay@gmail.com>
2018-10-26 22:18:42 -0400

    add homeComponent

---
+++ src/homeComponent/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class home extends Component {
+    render() {
+        return (
+            <h1>Green City Force</h1>
+            <div>
+                <h2>MISSION</h2>
+                <p>Green City Force’s (GCF) mission is to break the cycle of poverty, preparing urban young adults to succeed in their chosen careers by engaging them in service, training and work experiences related to the clean energy economy. In doing so, GCF encourages them to lead socially and environmentally responsible lives.</p>
+            </div>
+        );
+    }
+};
+
+export default home;
