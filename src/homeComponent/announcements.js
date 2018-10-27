dfb5f1090be83e2ec29e1b9247eaa90ba30d2154
LENOVO-PC\diana_000 <mariaj4867@gmail.com>
2018-10-27 09:03:18 -0400

    added code to announcements.js & css files

---
+++ src/homeComponent/announcements.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './announcements.css';
+
+class Announcements extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			items: []
+		}
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(e) {
+  		this.setState({
+    	[e.target.name]: e.target.value
+ 	 	});
+	}
+
+	render(){
+		const announcementsList = this.state.items.map(item => <li>{item}</li>)
+		return (
+			<div className = 'announcements'>
+				<header> 
+					<div className = 'wrapper'>
+						<h1 className="announcement"> Announcements </h1> 
+					</div>
+				</header>
+				<body>
+					<div class = 'green-box'>
+						<h3>Updates about today's evening yoga class at 6:30 pm</h3>
+						<p>Class is cancelled</p>
+					</div>
+					<div class = 'green-box'>
+						<h3>Exciting news about opening another farm location in West 
+							8th Street, Brooklyn area</h3>
+							<p>Please, come, check it out, and volunteer at our recently
+								opened farm in Brooklyn!</p>
+					</div>
+				</body>
+			</div>
+		);
+  }  
+}
+export default Announcements;
--- src/homeComponent/news.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-class Newsfeed extends React.Component {
-    
-
-}
