378bca12402c8e63544a039c29cc046f4209764e
panncherry <maung.eainmatmay@gmail.com>
2018-10-26 23:00:15 -0400

    update index.css

--- src/homeComponent/home.js
+++ src/homeComponent/home.js
@@ -6,9 +6,10 @@ class home extends Component {
             <div>
 
             <h1>Green City Force</h1>
-            <div id= "mission">
+            <div id = "mission">
                 <h2>MISSION</h2>
                 <p>Green City Force’s (GCF) mission is to break the cycle of poverty, preparing urban young adults to succeed in their chosen careers by engaging them in service, training and work experiences related to the clean energy economy. In doing so, GCF encourages them to lead socially and environmentally responsible lives.</p>
+                <br></br>
             </div>
 
             <div id = "aboutus">
